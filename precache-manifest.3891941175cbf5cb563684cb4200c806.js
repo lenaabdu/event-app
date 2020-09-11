6adb490d5ea578aa0f35265c9ab98a1fe8cd5df4
lenaabdu <lenaabdu23@gmail.com>
2020-09-12 00:34:02 +0100

    Updates

---
+++ precache-manifest.3891941175cbf5cb563684cb4200c806.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7c10e4b9ea62f1ee62dabb81223d7f4",
+    "url": "/event-app/index.html"
+  },
+  {
+    "revision": "38fbdb3ad24e1125deb6",
+    "url": "/event-app/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "fc4d28df4d3765485911",
+    "url": "/event-app/static/css/main.bbccfe60.chunk.css"
+  },
+  {
+    "revision": "38fbdb3ad24e1125deb6",
+    "url": "/event-app/static/js/2.77b68cb5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/event-app/static/js/2.77b68cb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc4d28df4d3765485911",
+    "url": "/event-app/static/js/main.348aeba3.chunk.js"
+  },
+  {
+    "revision": "4a04ede833f70f6d8206",
+    "url": "/event-app/static/js/runtime-main.176b038f.js"
+  }
+]);
